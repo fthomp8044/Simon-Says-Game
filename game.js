7fe22918f58c5ca62467ee24371eeb2671203eca
fthomp8044 <fthomp2430@gmail.com>
2020-02-05 22:15:34 -0500

    added sounded and event to buttons

--- game.js
+++ game.js
@@ -1,7 +1,29 @@
 
 let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+$(".btn").click(function(){
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  //This will push any selected color btn to my array list
+  playSound(userChosenColour);
+
+  // console.log(userClickedPattern);
+})
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
 };
+
+function playSound(name){
+  let audio = new Audio("sounds/" + name + "mp3");
+  audio.play();
+}
